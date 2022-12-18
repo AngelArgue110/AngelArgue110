af887c03afc6cfcdf20f70bf074daea83e989070
AngelArgue110 <11amiguela29@gmail.com>
2022-12-18 21:11:41 +0000

    everything looped

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -48,14 +48,11 @@ var init = function (window) {
                 var eachCircle = circles[i];
                 
                 physikz.updatePosition(eachCircle);
+                game.checkCirclePosition(eachCircle)
              }
 
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition(circles[0])
-            game.checkCirclePosition(circles[1])
-            game.checkCirclePosition(circles[2])
-            game.checkCirclePosition(circles[3])
-            game.checkCirclePosition(circles[4])
+
             // TODO 9 : Iterate over the array
            
             
