a74648f009f42645e335ecfed31a53710e704707
AngelArgue110 <11amiguela29@gmail.com>
2022-12-18 21:06:27 +0000

    lotta circle movement

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -44,12 +44,12 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
+            for (var i = 0; i < circles.length; i++) {
+                var eachCircle = circles[i];
+                
+                physikz.updatePosition(eachCircle);
+             }
 
-            physikz.updatePosition(circles[0]);
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3]);
-            physikz.updatePosition(circles[4]);
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
             game.checkCirclePosition(circles[0])
             game.checkCirclePosition(circles[1])
